e4c8e238c31f92f2d77cdd97519e0f3c59f515a9
jatin chaturani <jatin.chaturani15@gmail.com>
2020-07-10 19:27:28 +0530

    Updates

---
+++ precache-manifest.b8033bd9f56cb5aab79b90141cb90962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af68f1387d13a60de3df58d316d2896",
+    "url": "/React-Recipe-Search-App/index.html"
+  },
+  {
+    "revision": "aa1a310b671db66e3ab6",
+    "url": "/React-Recipe-Search-App/static/css/main.beae61bf.chunk.css"
+  },
+  {
+    "revision": "305b38dd4a3e589885f2",
+    "url": "/React-Recipe-Search-App/static/js/2.b9cb2ab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe-Search-App/static/js/2.b9cb2ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1a310b671db66e3ab6",
+    "url": "/React-Recipe-Search-App/static/js/main.76803153.chunk.js"
+  },
+  {
+    "revision": "3fd9a87231d981538fc5",
+    "url": "/React-Recipe-Search-App/static/js/runtime-main.f7ceec95.js"
+  }
+]);
